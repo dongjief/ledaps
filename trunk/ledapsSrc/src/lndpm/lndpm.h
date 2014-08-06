45430b2fb0da15da2742da9ef85f216570b5d936
schmidtga00@gmail.com <schmidtga00@gmail.com@10324028-1983-28d5-8f14-fde5f4c4070c>
2014-08-06 17:29:28 +0000

    Updated the version number for the latest changes.

--- trunk/ledapsSrc/src/lndpm/lndpm.h
+++ trunk/ledapsSrc/src/lndpm/lndpm.h
@@ -32,7 +32,7 @@ NOTES:
 
 /* Defines */
 /* LEDAPS VERSION definitions */
-#define LEDAPS_VERSION "2.0.0"
+#define LEDAPS_VERSION "2.0.1"
 #define logFile "LogReport"
 
 /* define useful constants */
